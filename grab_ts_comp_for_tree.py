c132b9b43ac6be6505d83c16a48ec5ca25a0f46b
Tom Xu <txu@guidehouse.com>
2022-09-27 21:12:25 -0400

    added series2supervised.py

--- grab_ts_comp_for_tree.py
+++ grab_ts_comp_for_tree.py
@@ -1,4 +1,8 @@
-SIZE = 60 #we take 2 past months here for each time-series point
+import pandas as pd
+import numPy as np
+
+
+""" SIZE = 60 #we take 2 past months here for each time-series point
 COLUMNS = ['t{}'.format(x) for x in range(SIZE)] + ['target']
 df_train = []
 for i in range(SIZE, df.shape[0]):
@@ -47,4 +51,46 @@ params = {
 }
 x_train = lgb.Dataset(df_feats, df_train['target'])
 model = lgb.train(params, x_train, num_boost_round=500)
-preds = model.predict(df_feats_validation)
+preds = model.predict(df_feats_validation) """
+
+
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+	"""
+	Frame a time series as a supervised learning dataset.
+	Arguments:
+		data: Sequence of observations as a list or NumPy array.
+		n_in: Number of lag observations as input (X).
+		n_out: Number of observations as output (y).
+		dropnan: Boolean whether or not to drop rows with NaN values.
+	Returns:
+		Pandas DataFrame of series framed for supervised learning.
+	"""
+	n_vars = 1 if type(data) is list else data.shape[1]
+	df = pd.DataFrame(data)
+	cols, names = list(), list()
+	# input sequence (t-n, ... t-1) where n is the lag
+	for i in range(n_in, 0, -1):
+		cols.append(df.shift(i))
+		names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
+	# forecast sequence (t, t+1, ... t+n)
+	for i in range(0, n_out):
+		cols.append(df.shift(-i))
+		if i == 0:
+			names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
+		else:
+			names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
+	# put it all together
+	agg = concat(cols, axis=1)
+	agg.columns = names
+	# drop rows with NaN values
+	if dropnan:
+		agg.dropna(inplace=True)
+	return agg
+
+
+raw = pd.DataFrame()
+raw['ob1'] = [x for x in range(10)]
+raw['ob2'] = [x for x in range(50, 60)]
+values = raw.values
+data = series_to_supervised(values, 1, 2)
+print(data)
